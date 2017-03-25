55f59e03ed6c6fa97c15a49165d35a26e4986c42
Xing Hu <xinghu0164@gmail.com>
2017-03-25 17:37:36 +1100

    add grid class

---
+++ src/Process/Grid.java
@@ -0,0 +1,52 @@
+package Process;
+
+public class Grid {
+
+	public String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public float xmin;
+	public float xmax;
+	public float ymin;
+	public float ymax;
+
+	public float getXmin() {
+		return xmin;
+	}
+
+	public void setXmin(float xmin) {
+		this.xmin = xmin;
+	}
+
+	public float getXmax() {
+		return xmax;
+	}
+
+	public void setXmax(float xmax) {
+		this.xmax = xmax;
+	}
+
+	public float getYmin() {
+		return ymin;
+	}
+
+	public void setYmin(float ymin) {
+		this.ymin = ymin;
+	}
+
+	public float getYmax() {
+		return ymax;
+	}
+
+	public void setYmax(float ymax) {
+		this.ymax = ymax;
+	}
+
+}
